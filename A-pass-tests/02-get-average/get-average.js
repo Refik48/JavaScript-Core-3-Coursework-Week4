bd592b39cdd5844393be249087290ca6fe28b640
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-11-24 23:44:23 +0000

    A-pass-tests done

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,10 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let numFilter = numbers.filter((x) => typeof x == "number");
 
+  return numFilter.reduce((a, b) => a + b, 0) / numFilter.length;
+}
+console.log(average([4, "-", 8, 11, "hello", "57", 0, 2]));
 module.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,10 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let obj = {};
 
+  carsSold.map((car) => {
+    obj[car.make] ? (obj[car.make] += car.price) : (obj[car.make] = car.price);
+  });
+
+  return obj;
+}
 module.exports = sales;
